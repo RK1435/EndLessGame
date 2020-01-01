7e89272ab92c46fe3aed0ea5ce595fdcb4623bd4
RK1435 <46847867+RK1435@users.noreply.github.com>
2020-01-01 07:37:03 +0530

    Add files via upload

---
+++ Scripts/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            //Increase Score of 1!!!
+            score++;
+            Debug.Log(score);
+        }
+    }
+}
